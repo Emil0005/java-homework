d4e799d8ae6191b728d4faa4cd1655c1e89282bf
Emil0005 <94788537+Emil0005@users.noreply.github.com>
2021-12-11 23:32:33 +0400

    Add files via upload

---
+++ src/Week2/Task1_1.java
@@ -0,0 +1,13 @@
+package Week2;
+
+public class Task1_1{
+    public static void main(String args[]){
+
+        int a[]={12,2,5,8,7,9,13,46};
+        
+        for(int i=0;i<a.length;i++){
+            if(a[i]%2==0){
+                System.out.println(a[i]);
+            }
+        }
+    }}
